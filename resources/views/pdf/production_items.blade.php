cfe0e946b88700f4d223c19ba9fcbf2666d728e8
Carlo Keysser <mail@carlokeysser.de>
2026-05-29 09:51:06 +0200

    Update PDF export for camera config relations

--- resources/views/pdf/production_items.blade.php
+++ resources/views/pdf/production_items.blade.php
@@ -51,7 +51,21 @@
             @foreach ($cameraConfigs as $config)
             <tr>
             <td><strong>{{ $config->item->bezeichnung }} {{ $config->item->nummer }}</strong><br>
-            <em>Objektiv:</em> {{ $config->lens ?? '/' }}<br>
+            <em>Objektiv:</em> {{ $config->lensItem->bezeichnung ?? '/' }}
+                            @isset($config->lensItem->nummer)
+                            <span class="font-bold"> Nr.{{ $config->lensItem->nummer }}</span>
+                            @endisset
+            <br>
+            <em>Adapter:</em> {{ $config->adapterItem->bezeichnung ?? '/' }}
+            @isset($config->adapterItem->nummer)
+            <span class="font-bold"> Nr.{{ $config->adapterItem->nummer }}</span>
+            @endisset
+            <br>
+            <em>Stativkopf:</em> {{ $config->headItem->bezeichnung ?? '/' }}
+            @isset($config->headItem->nummer)
+            <span class="font-bold"> Nr.{{ $config->headItem->nummer }}</span>                                                        
+            @endisset
+            <br>    
             <em>Stativ:</em> {{ $config->tripod ?? '/' }}<br>
             <em>Position:</em> {{ $config->cam_position ?? '/' }}</td>
             <td>ToDo: Notes einbinden</td>
@@ -62,3 +76,51 @@
     </table>
 </body>
 </html>
+
+
+<<!--  @foreach ($cameraConfigs as $config)
+                    <tr class="even:bg-green-200">
+                        <td class="text-left w-12 pl-4">
+                            <a href="{{ route('productions.pdf', $config->production->id) }}" class="btn btn-primary" title="PDF Exportieren">
+                                <i class="text-left text-red-500 fas fa-file-pdf"></i>
+                            </a>
+                        </td>
+                        <td class="text-left pl-4">{{ $config->production->bezeichnung }}</td>
+                        <td class="text-left pl-4">
+                            <strong>Kamera:</strong> {{ $config->item->bezeichnung ?? '/' }}
+                            @isset($config->item->nummer)
+                            <span class="font-bold">{{ $config->item->nummer }}</span>
+                            @endisset
+                            <br>
+
+                            <strong>Objektiv:</strong> {{ $config->lensItem->bezeichnung }}
+                            @isset($config->lensItem->nummer)
+                            <span class="font-bold">{{ $config->lensItem->nummer }}</span>
+                            @endisset
+                            <br>
+
+                            <strong>Largelens-Adapter:</strong> {{ $config->adapterItem->bezeichnung ?? '/' }}
+                            @isset($config->adapterItem->nummer)
+                            <span class="font-bold">{{ $config->adapterItem->nummer }}</span>
+                            @endisset
+                            <br>
+
+                            <strong>Stativkopf:</strong> {{ $config->headItem->bezeichnung ?? '/' }}
+                            @isset($config->headItem->nummer)
+                            <span class="font-bold">{{ $config->headItem->nummer }}</span>                                                        
+                            @endisset
+                            <br>
+
+                            <strong>Stativ:</strong> {{ $config->tripodItem->bezeichnung ?? '/' }}
+                            @isset($config->tripodItem->nummer)
+                            <span class="font-bold">{{ $config->tripodItem->nummer }}</span>                                                        
+                            @endisset
+                            <br>
+
+                            <strong>Position:</strong> {{ $config->cam_position ?? '/' }}
+                        </td>
+                        <td class="text-left pl-4">{{ $config->item->unit->bezeichnung ?? '/' }}</td>
+                    </tr>
+                    @endforeach -->
+
+                    
